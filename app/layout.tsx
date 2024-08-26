fcafc005418c9e338962badf121af667721cf88d
nicorubiotsx <nrubioolivares@gmail.com>
2024-08-26 02:30:08 -0400

    changes advanced with nav responsive ok3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Gotu, Roboto_Mono,Playfair_Display } from "next/font/google";
+import { Inter, Gotu, Roboto_Mono,Playfair_Display,Merriweather } from "next/font/google";
 import "./globals.css";
 import 'animate.css';
 import Footer from "./ui/Footer";
@@ -8,7 +8,7 @@ import Navbar from "./ui/navbar/Navbar";
 
 const Ro =Roboto_Mono({ weight: "400", subsets: ["latin"] })
 const play =Playfair_Display({ weight: "400", subsets: ["latin"] })
-
+const merri = Merriweather({ weight: "400", subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Centro Estetica Anais",
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
             
-      <body className={play.className}>
+      <body className={merri.className}>
         <Navbar/>
         {children}
 
