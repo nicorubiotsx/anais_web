2c9a34817ff88a57d38dc6e6effdbcd008789d39
nicorubiotsx <nrubioolivares@gmail.com>
2024-08-26 01:32:39 -0400

    changes advanced with nav responsive ok

--- app/ui/navbar/Navbar.tsx
+++ app/ui/navbar/Navbar.tsx
@@ -2,17 +2,24 @@
 import { useState } from 'react';
 import './styles.module.css';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname()
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  
+
+  // Function to determine if a link is active
+  const isActive = (path: string) => pathname === path ? 'text-pink-600 bg-gray-800' : 'text-pink-400';
+
   return (
     <>
-      <nav className="bg-black text-white p-4 fixed top-0 left-0 w-full z-50">
+      <nav className="bg-black text-white p-4 fixed top-0 left-0 w-full z-50 shadow-lg border-b-2 border-pink-400">
         <div className="container mx-auto flex justify-between items-center">
           {/* Logo */}
           <div className="text-2xl font-bold">
@@ -29,12 +36,12 @@ export default function Navbar() {
           </div>
 
           {/* Menu Items */}
-          <div className={`lg:flex lg:items-center lg:space-x-6 absolute lg:relative top-full left-0 w-full lg:w-auto bg-black lg:bg-transparent ${isOpen ? 'block' : 'hidden'}`}>
-            <Link href="/" className="text-pink-400 block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3">Inicio</Link>
-            <Link href="/acerca" className="text-pink-400 block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3">Acerca</Link>
-            <Link href="/tratamiento" className="text-pink-400 block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3">Tratamientos</Link>
-            <Link href="/pregunta" className="text-pink-400 block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3">Preguntas</Link>
-            <Link href="/galeria" className="text-pink-400 block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3">Galería</Link>
+          <div className={`lg:flex lg:items-center lg:space-x-6 absolute lg:relative top-full left-0 w-full lg:w-auto bg-black lg:bg-transparent ${isOpen ? 'block' : 'hidden'} shadow-lg border-t-2 border-pink-400 lg:border-none`}>
+            <Link href="/" className={`block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3 ${isActive('/')}`}>Inicio</Link>
+            <Link href="/acerca" className={`block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3 ${isActive('/acerca')}`}>Acerca</Link>
+            <Link href="/tratamiento" className={`block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3 ${isActive('/tratamiento')}`}>Tratamientos</Link>
+            <Link href="/pregunta" className={`block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3 ${isActive('/pregunta')}`}>Preguntas</Link>
+            <Link href="/galeria" className={`block px-4 py-2 text-lg hover:bg-gray-700 lg:text-xl lg:px-6 lg:py-3 ${isActive('/galeria')}`}>Galería</Link>
           </div>
         </div>
       </nav>
